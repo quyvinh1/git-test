2a861b27526041d0583e35301a404c55344ef7ea
quyvinh1 <155459501+quyvinh1@users.noreply.github.com>
2024-03-05 18:51:12 +0700

    Update ManagerServlet.java

--- src/java/controller/ManagerServlet.java
+++ src/java/controller/ManagerServlet.java
@@ -95,7 +95,7 @@ public class ManagerServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request,response);
+        
     }
 
     /**
